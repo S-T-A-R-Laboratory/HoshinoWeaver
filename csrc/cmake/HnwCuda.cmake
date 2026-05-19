963fe18f0c7da95a1a26497bbff107e1d218d036
Chennes Xu <xuchen359@gmail.com>
2026-05-19 11:07:55 +0800

    feat: auto-detect CUDA architectures based on toolkit version

--- csrc/cmake/HnwCuda.cmake
+++ csrc/cmake/HnwCuda.cmake
@@ -1,8 +1,24 @@
 macro(hnw_enable_cuda_language)
     if(HNW_ENABLE_CUDA)
-        enable_language(CUDA)
-        include(CMakeCUDAInformation)
+        # Detect toolkit version first to choose architectures
         find_package(CUDAToolkit REQUIRED)
+
+        if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+            # Pascal (60) through latest supported by detected toolkit
+            set(CMAKE_CUDA_ARCHITECTURES "60;70;75;80;86")
+            if(CUDAToolkit_VERSION VERSION_GREATER_EQUAL "11.8")
+                list(APPEND CMAKE_CUDA_ARCHITECTURES 89)
+            endif()
+            if(CUDAToolkit_VERSION VERSION_GREATER_EQUAL "12.0")
+                list(APPEND CMAKE_CUDA_ARCHITECTURES 90)
+            endif()
+            if(CUDAToolkit_VERSION VERSION_GREATER_EQUAL "12.8")
+                list(APPEND CMAKE_CUDA_ARCHITECTURES 100)
+            endif()
+            message(STATUS "CUDA architectures (auto): ${CMAKE_CUDA_ARCHITECTURES}")
+        endif()
+
+        enable_language(CUDA)
     endif()
 endmacro()
 
